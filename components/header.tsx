a15ab9c7fc821d357d62d5db8180e23d45ed0d5f
f7deat <tandc.it4@vnpt.vn>
2022-03-23 15:15:19 +0700

    change logo

--- components/header.tsx
+++ components/header.tsx
@@ -2,23 +2,27 @@ import Image from "next/image";
 import styles from "../styles/home.module.css";
 
 const Header = () => {
-    return (
-        <header className={styles.header}>
-        <div className={styles.logo}>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={150} height={33} />
-        </div>
-        <div className={styles.menu}>
-          <ul>
-            <li className={styles.active}>Cửa hàng</li>
-            <li>Liên hệ</li>
-            <li>Workshops</li>
-            <li>
-              <a href="https://defzone.net">Blogs</a>
-            </li>
-          </ul>
-          <div className=""></div>
-        </div>
-      </header>
-    )
-}
-export default Header
+  return (
+    <header className="flex items-center justify-between px-2 shadow">
+      <div className="flex gap-2 items-center text-gray-800 px-2">
+        <i className="fas fa-shield-alt text-2xl"></i>
+        <div className="font-bold text-xl">DefZone<span className="text-primary">.Net</span></div>
+      </div>
+      <div className={styles.menu}>
+        <ul>
+          <li className={styles.active}>Cửa hàng</li>
+          <li>Liên hệ</li>
+          <li>Workshops</li>
+          <li>
+            <a href="https://defzone.net">Blogs</a>
+          </li>
+        </ul>
+        <div className=""></div>
+      </div>
+      <button className="h-10 w-10 border border-gray-200 flex items-center justify-center rounded hover:bg-gray-100 block md:hidden">
+        <i className="fas fa-bars"></i>
+      </button>
+    </header>
+  );
+};
+export default Header;
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,66 +1,82 @@
-import { useState } from 'react';
-import styles from '../styles/product-list.module.css';
-import { Product } from '../types/product';
+import { useState } from "react";
+import styles from "../styles/product-list.module.css";
+import { Product } from "../types/product";
 
 interface IProductListProps {
-    products: Product[];
-    product: any;
+  products: Product[];
+  product: any;
 }
 
 export const ProductList = (props: IProductListProps) => {
+  const [searchTerm, setSearchTerm] = useState("Monstera");
 
-    const [searchTerm, setSearchTerm] = useState('Monstera')
+  const currencyFormat = (input: string) =>
+    new Intl.NumberFormat("vi-VN", {
+      style: "currency",
+      currency: "VND",
+    }).format(Number(input));
 
-    const currencyFormat = (input: string) => new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(Number(input));
-
-    return (
-        <div className={styles.productList}>
-            <div className={styles.search}>
-                <div className={styles.searchControl}>
-                    <button className={styles.searchButton}><i className="fas fa-search"></i></button>
-                    <input type="text" className={styles.searchInput} placeholder="Nhập từ khóa..." onChange={(e) => setSearchTerm(e.target.value)} value={searchTerm} />
-                </div>
-                <div className={styles.searchKeyword}>
-                    <span className={styles.gray}>Kết quả tìm kiếm cho </span>
-                    <span className={styles.keyword}>“{searchTerm}”</span>
-                </div>
-            </div>
-            <div className={styles.sort}>
-                <span className="label">Sắp xếp</span>
-                <button className={`${styles.btnSort} ${styles.active}`}>Liên quan</button>
-                <button className={styles.btnSort}>Mua nhiều</button>
-                <button className={styles.btnSort}>Mới nhất</button>
-                <button className={styles.btnSort}>Giá</button>
-            </div>
-            <div className={styles.list}>
-                {
-                    props.products?.map((product: Product, index: number) => (
-                        <div className={styles.item} key={index}>
-                            <div className={styles.card}>
-                                <button className={styles.like} type="button"><i className="fas fa-heart"></i></button>
-                                <div className={styles.thumbnail}>
-                                    <img src={product.thumbnail} alt={product.name} />
-                                </div>
-                                <div className={styles.cardBody}>
-                                    <div className={styles.rate}>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                    </div>
-                                    <div className={styles.name}>{product.name}</div>
-                                </div>
-                                <div className={styles.action}>
-                                    <div className={styles.price}>{currencyFormat(product.regularPrice)}</div>
-                                    <button className={styles.addToCart}><i className="fas fa-cart-plus mr-2"></i>Thêm vào giỏ</button>
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                }
-
-            </div>
+  return (
+    <div className={styles.productList}>
+      <div className={styles.search}>
+        <div className={styles.searchControl}>
+          <button className={styles.searchButton}>
+            <i className="fas fa-search"></i>
+          </button>
+          <input
+            type="text"
+            className={styles.searchInput}
+            placeholder="Nhập từ khóa..."
+            onChange={(e) => setSearchTerm(e.target.value)}
+            value={searchTerm}
+          />
         </div>
-    )
-}
+        <div className={styles.searchKeyword}>
+          <span className={styles.gray}>Kết quả tìm kiếm cho </span>
+          <span className={styles.keyword}>“{searchTerm}”</span>
+        </div>
+      </div>
+      <div className={styles.sort}>
+        <span className="label">Sắp xếp</span>
+        <button className={`${styles.btnSort} ${styles.active}`}>
+          Liên quan
+        </button>
+        <button className={styles.btnSort}>Mua nhiều</button>
+        <button className={styles.btnSort}>Mới nhất</button>
+        <button className={styles.btnSort}>Giá</button>
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        {props.products?.map((product: Product, index: number) => (
+          <div className="" key={index}>
+            <div className={styles.card}>
+              <div className={styles.thumbnail}>
+                <img src={product.thumbnail} alt={product.name} />
+              </div>
+              <div className="flex justify-between py-2 items-center">
+                <div className={styles.rate}>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                </div>
+                <button className="text-red-500 px-1" type="button">
+                  <i className="fas fa-heart"></i>
+                </button>
+              </div>
+              <div className={styles.name}>{product.name}</div>
+              <div className={styles.action}>
+                <div className="text-red-500 text-sm">
+                  {currencyFormat(product.regularPrice)}
+                </div>
+                <button className="px-4 py-1 border rounded-full transition duration-500">
+                  <i className="fas fa-cart-plus mr-2"></i>Thêm vào giỏ
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
